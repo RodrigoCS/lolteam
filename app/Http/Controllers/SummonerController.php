659046497c99ff827bf0692ffb9eec6fba833dbd
RodrigoCS <r.odrigo.cs.9n@gmail.com>
2016-06-14 03:57:25 -0500

    Mas info en summoner
    Optimizacion de SummonerController
    Migracion para añadir las columnas de club a la base de datos
    Mas informacion de el summoner / icono / liga etc..

--- app/Http/Controllers/SummonerController.php
+++ app/Http/Controllers/SummonerController.php
@@ -8,6 +8,7 @@ use App\Http\Requests;
 use Auth;
 
 
+
 function summoner_info_array_name($summoner) {
 	$summoner_lower = mb_strtolower($summoner, 'UTF-8');
 	$summoner_nospaces = str_replace(' ', '', $summoner_lower);
@@ -15,24 +16,45 @@ function summoner_info_array_name($summoner) {
 }
 
 
-
 class SummonerController extends Controller
 {
+    private $apiKey;
+    private $region;
+
+    public function __construct()
+    {
+        $this->apiKey = $_ENV['RIOT_API_KEY']; 
+        $this->region = Auth::user()->region;
+    }
+
+
+    public function getSummoner() {
+
+    }
+
+    public function getLeagueEntry($summonerId) {
+        $url = 'https://'.$this->region.'.api.pvp.net/api/lol/'.$this->region.'/v2.5/league/by-summoner/'.$summonerId.'/entry?api_key='.$this->apiKey;
+        $response = file_get_contents($url);
+        $leagueEntry =  json_decode($response);
+        return $leagueEntry->{$summonerId}[0];
+    }
+
     public function summoner(Request $request) {
-        
-            $summonerName = Auth::user()->summoner;
-            $region = Auth::user()->region;
-            $APIKey = "844dbefd-2dcc-420c-9f3a-4095fde86813";
-            $url = "https://lan.api.pvp.net/api/lol/lan/v1.4/summoner/by-name/$summonerName?api_key=844dbefd-2dcc-420c-9f3a-4095fde86813";
-            $url2 = "https://lan.api.pvp.net/api/lol/".$region."/v1.4/summoner/by-name/".$summonerName."?api_key=".$APIKey; 
-            $response = file_get_contents($url);
-            $summoner =  json_decode($response);
 
-            $summonerName = summoner_info_array_name($summonerName);
+        $summonerName = Auth::user()->summoner;
+        $url = 'https://'.$this->region.'.api.pvp.net/api/lol/'.$this->region.'/v1.4/summoner/by-name/'.$summonerName.'?api_key='.$this->apiKey;
 
-            $name = $summoner->{$summonerName}->{'name'};
-            $level= $summoner->{$summonerName}->{'summonerLevel'};
-            return view('summoner/summoner', ['name' => $name, 'level' => $level]);
+        $response = file_get_contents($url);
+        $summoner =  json_decode($response);
+        //$summonerName = summoner_info_array_name($summonerName);
+
+        $name = $summoner->{$summonerName}->{'name'};
+        $level= $summoner->{$summonerName}->{'summonerLevel'};
+        $leagueEntry = $this->getLeagueEntry($summoner->{$summonerName}->{'id'});
+
+
+        //dd($leagueEntry->{'entries'}[0]->{'division'});
+        return view('summoner/summoner', ['summoner' => $summoner->{$summonerName}, 'leagueEntry' => $leagueEntry]);
 
         //return redirect()->route('summoner/search/{region}/{summonerName}', ['location' => $location, 'search' => $search]);
     }
--- resources/views/summoner/summoner.blade.php
+++ resources/views/summoner/summoner.blade.php
@@ -5,12 +5,33 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Summoner: {{Auth::user()->summoner}}</div>
+                <div class="panel-heading">Summoner: {{ $summoner->name }}</div>
 
                 <div class="panel-body">
-                    You are logged in!
-                    <hr>
-                    {{$name}} -- {{$level}}
+
+                    <div class="media">
+                      <div class="media-left">
+                        <a href="#">
+                          <img class="media-object img-thumbnail" src="{{ asset('/assets/images/profileIcons/profileIcon'.$summoner->profileIconId.'.jpg') }}" alt="{{ $summoner->name }}">
+                        </a>
+                      </div>
+                      <div class="media-body">
+                        <h4 class="media-heading">{{ $summoner->name }} <span class="summoner_tag">{{Auth::user()->club_tag}}</span></h4>
+                        <span>Level: </span>{{ $summoner->summonerLevel }}
+                        <div>{{ $leagueEntry->queue }}</div>
+                        {{ $leagueEntry->name }}
+                        <div>
+                            {{ $leagueEntry->tier }}
+                            {{ $leagueEntry->entries[0]->{'division'} }} >
+                            {{ $leagueEntry->entries[0]->{'leaguePoints'} }} LP
+                        </div>
+                        <span class="text-success">{{ $leagueEntry->entries[0]->{'wins'} }}</span>
+                        /<span class="text-danger">{{ $leagueEntry->entries[0]->{'losses'} }}</span>
+                        > {{ round(100/((($leagueEntry->entries[0]->{'wins'})+($leagueEntry->entries[0]->{'losses'}))/($leagueEntry->entries[0]->{'wins'})), 2) }} %
+
+                      </div>
+                    </div>
+
                 </div>
             </div>
         </div>
